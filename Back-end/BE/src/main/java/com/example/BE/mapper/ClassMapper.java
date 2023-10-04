6679802073379b3487cfca383ed5791f95990451
HaiquangPham14 <phamwang02@gmail.com>
2023-10-04 15:29:58 +0700

    view Class List
    view Class List

---
+++ Back-end/BE/src/main/java/com/example/BE/mapper/ClassMapper.java
@@ -0,0 +1,28 @@
+package com.example.BE.mapper;
+
+import com.example.BE.model.entity.Class;
+import com.example.BE.model.dto.ClassDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+@Mapper(componentModel = "spring")
+public interface ClassMapper {
+    @Mapping(source = "class1.classID", target = "classID")
+    @Mapping(source = "class1.training_code", target = "training_code")
+    @Mapping(source = "class1.className", target = "className")
+    @Mapping(source = "class1.classCode", target = "classCode")
+    @Mapping(source = "class1.duration", target = "duration")
+    @Mapping(source = "class1.status", target = "status")
+    @Mapping(source = "class1.location", target = "location")
+    @Mapping(source = "class1.fsu", target = "fsu")
+    @Mapping(source = "class1.start_date", target = "start_date")
+    @Mapping(source = "class1.end_date", target = "end_date")
+    @Mapping(source = "class1.create_by", target = "create_by")
+    @Mapping(source = "class1.createdDate", target = "createdDate")
+    @Mapping(source = "class1.modified_by", target = "modified_by")
+    @Mapping(source = "class1.modified_date", target = "modified_date")
+    ClassDTO toClassDTO(Class class1);
+
+    List<ClassDTO> toClassDTOList(List<Class> classes);
+}
