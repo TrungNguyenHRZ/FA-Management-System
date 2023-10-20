f8d09efa6d7c868c15b5d093f788deab7d82b7ff
bao <baocao494@gmail.com>
2023-10-20 08:41:31 +0700

    add object

--- Back-end/BE/src/main/java/com/example/BE/model/dto/response/LearningObjectiveResponse.java
+++ Back-end/BE/src/main/java/com/example/BE/model/dto/response/LearningObjectiveResponse.java
@@ -1,7 +1,13 @@
 package com.example.BE.model.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LearningObjectiveResponse {
 	
+	private int learningCode;
 	private String name;
 	private String description;
 }
