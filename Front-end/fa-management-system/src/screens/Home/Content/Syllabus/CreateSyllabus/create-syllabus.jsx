f2c3b480db6852f8f60e21bf170dfb66dd5f4d63
bao <baocao494@gmail.com>
2023-12-09 22:50:47 +0700

    up

--- Front-end/fa-management-system/src/screens/Home/Content/Syllabus/CreateSyllabus/create-syllabus.jsx
+++ Front-end/fa-management-system/src/screens/Home/Content/Syllabus/CreateSyllabus/create-syllabus.jsx
@@ -1139,7 +1139,7 @@ const CreateSyllabus = () => {
                                   ],
                                 };
                               }
-                            );
+                          });
 
                             return {
                               ...prevValues,
@@ -1211,7 +1211,7 @@ const CreateSyllabus = () => {
                 </Modal>
                   <Button
                     className="form-create-syllabus-submit"
-                    type="submit"
+                    type="button"
                     onClick={() => {
                       switchPage();
                     }
@@ -1311,6 +1311,7 @@ const CreateSyllabus = () => {
                               progress: undefined,
                               theme: "light",
                             });
+                            handleSubmit(values);
                           }
                         });
                         console.log(values.publish_status);
